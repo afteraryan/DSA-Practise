a8a6fbeeb235e6d1c05ab396e9e31b0965c6f89f
afteraryan <saxenaa039@gmail.com>
2022-03-17 23:47:18 +0530

    All Negative Elements on 1 side

---
+++ Array/NegativeSide.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void ArrayNegative(int arr[],int n)
+{
+    int negativelocation=0,temp,j=0;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]<0)
+        {
+            if( i != j)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;                
+            }
+            j++;
+        }
+    }
+}
+
+
+int main() 
+{
+	//code
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	ArrayNegative(arr,n);
+	for(int i=0; i<n;i++)
+	{
+	    cout<<arr[i]<<",";
+	}
+	return 0;
+}
